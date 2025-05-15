e854d4100127b4b6e05656227b92ddaaeeed069b
Jake B <jbuntin4@uwo.ca>
2025-05-15 16:07:25 -0700

    fix remove unused style sheet

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jakeb99.github.io/',
   plugins: [
     react(),
     tailwindcss(),
